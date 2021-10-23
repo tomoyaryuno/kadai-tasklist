dfda223278a1df8f29b097844c23ec65743c0057
soran <soran@192.168.11.2>
2021-10-23 20:32:32 +0900

    getAllMessagesを追加

--- src/models/tasks.java
+++ src/models/tasks.java
@@ -7,11 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
-public class Message {
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM tasks AS m ORDER BY m.id DESC"
+    )
+})
+@Table(name = "tasks")
+public class tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
