233d221d065667e7a8646a01d9958f9e97ca7b08
soran <soran@192.168.11.2>
2021-10-24 09:48:59 +0900

    NewServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -34,7 +34,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<tasks> tasks = em.createNamedQuery("getAllMessages", tasks.class).getResultList();
+        List<tasks> tasks = em.createNamedQuery("getAlltasks", tasks.class).getResultList();
         response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
--- src/models/tasks.java
+++ src/models/tasks.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
+        name = "getAlltasks",
         query = "SELECT m FROM tasks AS m ORDER BY m.id DESC"
     )
 })
